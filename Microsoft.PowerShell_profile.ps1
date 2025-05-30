37d7a2bff2e696ff8cc343d82a71c31ef19bffcf
Gelson Pereira <gelsonpereira.ao@gmail.com>
2025-05-30 18:06:58 -0300

    segundo commit do oh-my-posh

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+oh-my-posh init pwsh --config 'C:\Users\Gelson Pereira\posh/34ohmyposh.json' | Invoke-Expression
+
+
+Import-Module PSReadLine
+
+#Import-Module PSColors
+#Import-Module posh-git
+Import-Module -Name Terminal-Icons
+Set-PSReadLineOption -PredictionViewStyle ListView
+#Import-Module oh-my-posh
+Import-Module z
+Import-Module Terminal-Icons
